e29c408ca4bfc6e1a0ad614dcce1e6f93bb508ce
khairi <khairi.yusoff@qumonintelligence.com>
2023-11-11 23:48:49 +0800

    update key

--- Jobs/hook/useFetch.js
+++ Jobs/hook/useFetch.js
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { RAPID_API_KEY } from "@env";
 
-const useFetch = () => {
+const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = ({ endpoint }) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const options = {
     method: "GET",
-    url: "https://jsearch.p.rapidapi.com/search",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
     params: {
       query: "Python developer in Texas, USA",
       page: "1",
       num_pages: "1",
-    },
-    headers: {
-      "X-RapidAPI-Key": "5dde110088msh19900a44b4bf838p1708e2jsnf7608eb4875e",
-      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
   };
   return <div>useFetch</div>;
